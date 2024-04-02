f72f268a4b60414de8013c26af279d8c99e26555
joyahmed <joy_ahmed_007@yahoo.com>
2024-04-03 03:29:15 +0600

    ✅Custom Component

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,35 +1,14 @@
 import products from '@/assets/data/products';
-import Colors from '@/src/constants/Colors';
-import { Image, StyleSheet, Text, View } from 'react-native';
+import ProductListItem from '@/src/components/ProductListItem';
 
-const product = products[0];
-
-const TabOneScreen = () => {
+const MenuScreen = () => {
 	return (
-		<View style={styles.container}>
-			<Image source={{ uri: product.image }} style={styles.image} />
-			<Text style={styles.title}>{product.name}</Text>
-			<Text style={styles.price}>${product.price}</Text>
-		</View>
+		<>
+			{products.map(product => (
+				<ProductListItem key={product.id} {...{ product }} />
+			))}
+		</>
 	);
 };
 
-export default TabOneScreen;
-
-const styles = StyleSheet.create({
-	container: {
-		backgroundColor: 'white',
-		padding: 10,
-		borderRadius: 20
-	},
-	image: {
-		width: `100%`,
-		aspectRatio: 1
-	},
-
-	title: { fontSize: 18, fontWeight: '700', marginVertical: 10 },
-	price: {
-		color: Colors.light.tint,
-		fontWeight: 'bold'
-	}
-});
+export default MenuScreen;
---
+++ types.d.ts
@@ -0,0 +1,8 @@
+interface ProductProps {
+	id: number;
+	name: string;
+	image: string;
+	price: number;
+}
+
+type PizzaSizeProps = 'S' | 'M' | 'L' | 'XL';
